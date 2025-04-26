d3e6f07d9ca8e3bc94992e8e0f20403baec98b04
supduupa <98172452+supduupa@users.noreply.github.com>
2025-04-25 19:56:50 -0700

    Add files via upload

---
+++ pro/script.js
@@ -0,0 +1,12 @@
+supa = document.getElementById("btn");
+
+function myfunction(){
+    
+    // document.body.style.backgroundColor = "black";
+    // supa.innerText = "Light mode";
+    document.getElementById("body").style.backgroundColor = "black";
+}
+
+function myfunction1(){
+    document.getElementById("body").style.backgroundColor = "white";
+}
